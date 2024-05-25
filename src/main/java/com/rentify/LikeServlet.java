665a2a5b76bd5ecc40fe5961cdb48e2d5c2a3068
5110Monica <114003759+5110Monica@users.noreply.github.com>
2024-05-25 11:08:34 +0530

    Update LikeServlet.java

--- src/main/java/com/rentify/LikeServlet.java
+++ src/main/java/com/rentify/LikeServlet.java
@@ -1,1 +1,22 @@
+package com.rentify;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class LikeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int propertyId = Integer.parseInt(request.getParameter("propertyId"));
+        PropertyService.likeProperty(propertyId);
+
+        Property property = PropertyService.getPropertyById(propertyId);
+        if (property != null) {
+            response.setContentType("application/json");
+            response.getWriter().write("{\"status\":\"success\", \"likeCount\":" + property.getLikeCount() + "}");
+        } else {
+            response.setContentType("application/json");
+            response.getWriter().write("{\"status\":\"error\"}");
+        }
+    }
+}
 
